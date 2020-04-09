b138af17339aadc4162a37cd5ef06d8e49f5ff52
古风 <42372340+UUsome@users.noreply.github.com>
2020-04-10 01:08:54 +0800

    Update settings.py
    DB修改

--- ThinkStore/settings.py
+++ ThinkStore/settings.py
@@ -84,25 +84,25 @@ WSGI_APPLICATION = 'ThinkStore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'thinkstore_git',
-        #'HOST': '127.0.0.1',
-       # 'HOST': '192.168.211.134',
-        'HOST': '39.96.204.75',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'thinkstore_git',
+#        #'HOST': '127.0.0.1',
+#       # 'HOST': '192.168.211.134',
+#        'HOST': '39.96.204.75',
+#        'USER': 'root',
+#        'PASSWORD': 'root',
+#        'PORT': '3306',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
